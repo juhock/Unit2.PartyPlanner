7feab7750be84f25fad3d73cb388708da6cb46e6
Julia Arfinengo <juliaarfinengo@Julias-MacBook-Pro-2.local>
2023-10-04 11:18:52 -0500

    SOLUTION

--- partyPlan.js
+++ partyPlan.js
@@ -1,39 +1,95 @@
-// Function to handle form submission
-function handleFormSubmit(event) {
-    event.preventDefault(); // Prevent the form from submitting default 
+//set up variables
 
-    // Get the form input values
-    const partyName = document.getElementById('party-name').value;
-    const partyDate = document.getElementById('party-date').value;
-    const partyTime = document.getElementById('party-time').value;
-    const partyLocation = document.getElementById('party-location').value;
-    const partyDescription = document.getElementById('party-description').value;
+const EVENTS_API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2309-FSA-ET-WEB-FT-SF/events`
 
-    // Create a new party object with input values
-    const newParty = {
-        name: partyName,
-        date: partyDate,
-        time: partyTime,
-        location: partyLocation,
-        description: partyDescription,
-    };
-
-    // POST request to add the new party to the API
-    fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2309FSA/events', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newParty),
-    })
-        .then(response => response.json())
-        .then(data => {
-        })
-        .catch(error => {
-            console.error('Error adding new party:', error);
-        });
+const state = {
+    parties: []
 }
 
-// Event listener for the form submission
-const partyForm = document.getElementById('party-form');
-partyForm.addEventListener('submit', handleFormSubmit);
+const partiesForm = document.querySelector('#newPartyForm')
+partiesForm.addEventListener('submit', addParty)
+const partiesList = document.querySelector('#partyList')
+
+//function to get all parties from the API
+
+async function getPartyInfo() {
+    try{
+        const response = await fetch (EVENTS_API_URL);
+        const json = await response.json();
+        state.parties = json.data;
+        console.log(json.data)
+    } catch(err){
+        console.log(err)
+    }
+}
+
+getPartyInfo()
+
+//function to add a single party using the information that has been entered into the form
+// make sure this function re-fetches the data and re-renders the new list
+
+async function createParty(name, description, date, location) {
+    const response = await fetch(EVENTS_API_URL, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            name, description, date, location
+        })
+    })
+    const json = await response.json()
+    console.log(json)
+}
+
+async function addParty(event){
+    event.preventDefault();
+    const date = new Date(partiesForm.date.value)
+    await createParty(
+        partiesForm.name.value, 
+        partiesForm.description.value, 
+        date.toISOString(), 
+        partiesForm.location.value)
+
+        partiesForm.name.value = ""
+        partiesForm.description.value = ""
+        partiesForm.date.value = ""
+        partiesForm.location.value = ""
+
+}
+
+//function to delete a single party
+// make sure this function re-fetches the data and re-renders the new list
+
+
+
+// function to render the list of all parties
+
+function renderAllParties() {
+    if (!state.parties.length) {
+        //update the list to say there aren't any
+        partiesList.innerHTML = `<li>No parties! Why don't you throw one?</li>`;
+        return;
+    }
+    const elements = state.parties.map(renderSingleParty)
+    partiesList.replaceChildren(...elements)
+}
+
+async function render() {
+    await getPartyInfo();
+    renderAllParties();
+}
+
+render();
+
+//function to render a single party 
+//single party render should also include a delete button which removes a party from the list when clicked by making a delete request to the server
+
+function renderSingleParty(party) {
+    const partyCard = document.createElement('section')
+    partyCard.classList.add("party-card")
+    partyCard.innerHTML = `<h2>${party.name}</h2>
+    <p>${party.date}</p>
+    <p>${party.location}</p>
+    <p>${party.description}</p>
+    `
+    return partyCard
+}
